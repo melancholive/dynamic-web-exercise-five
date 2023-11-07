9c723f3ef5803f10915ec95d586d1106a209027a
Si Yue Jiang <sj3834@nyu.edu>
2023-11-07 09:38:32 -0500

    firebase

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
 const express = require("express");
 const app = express();
+const firebase = require("firebase/app");
+
 const port = 4000;
+
+// web app's Firebase configuration -- make sure this is above the indexRoute
+const firebaseConfig = {
+    apiKey: "AIzaSyBkdgzUgpq-T_-ODdz33GwVPUSTwyUJvR0",
+    authDomain: "dynamic-web-exercise-fiv-c492d.firebaseapp.com",
+    projectId: "dynamic-web-exercise-fiv-c492d",
+    storageBucket: "dynamic-web-exercise-fiv-c492d.appspot.com",
+    messagingSenderId: "25901111321",
+    appId: "1:25901111321:web:867de02707e5bf72a8b288"
+  };
+
+// initialize firebase
+firebase.initializeApp(firebaseConfig);
 
 const indexRoute = require('./routes/index');
 // const singlePostRoute = require('./routes/singlePost');
 // const createPostRoute = require('./route/createPost');
-
+  
 app.use("/", indexRoute);
 
 app.listen(port, () => {
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,29 @@
 const express = require("express");
 const router = express.Router();
 
+// intialize firebase database
+const firestore = require("firebase/firestore");
+// create a reference to the database
+const db = firestore.getFirestore();
+
 // Define index route -- get all posts
 router.get("/", (req, res) => {
-    res.send("All Posts");
+    const postsQuery = firestore.getDocs(firestore.collection(db, "posts"));
+    const postsArray = [];
+
+    postsQuery
+        .then((response) => {
+            response.forEach((post) => {
+                console.log(post.data());
+                // ... is a spread operator, assigns the value throughout rather than creating individual objects for each key value pair
+                postsArray.push({id: post.id, ...post.data()}); 
+            });
+            res.send(postsArray);
+        }) // do not put semi colon -- they are connected
+        .catch((error) => {
+            console.log(error);
+            return res.send(error);
+        });
 });
 
 module.exports = router;
